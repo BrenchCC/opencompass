f0ed481ea8662891414f18dc7eb2ffc6cfb54667
Brench <148740663+BrenchCC@users.noreply.github.com>
2024-11-21 17:02:02 +0800

    Update eval_v1.py

--- configs/brench_eval_v1/eval_v1.py
+++ configs/brench_eval_v1/eval_v1.py
@@ -1,7 +1,7 @@
 from mmengine.config import read_base
 
 with read_base():
-    from ..datasets_collections.brench_eval_data_v1 import datasets
+    from ..dataset_collections.brench_eval_data_v1 import datasets
     from ..models.brench_eval_v1_models.qwen_model_v1 import models
 
 
