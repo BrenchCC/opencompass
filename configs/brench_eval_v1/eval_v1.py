64fa8b913e79063541739c42b5bdfcc2f5ae5ee6
Brench <148740663+BrenchCC@users.noreply.github.com>
2024-11-21 16:57:18 +0800

    Update eval_v1.py

--- configs/brench_eval_v1/eval_v1.py
+++ configs/brench_eval_v1/eval_v1.py
@@ -1,8 +1,8 @@
 from mmengine.config import read_base
 
 with read_base():
-    from opencompass.configs.datasets_collections.brench_eval_data_v1 import datasets
-    from opencompass.configs.models.brench_eval_v1_models.qwen_model_v1 import models
+    from ..datasets_collections.brench_eval_data_v1 import datasets
+    from ..models.brench_eval_v1_models.qwen_model_v1 import models
 
 
 
