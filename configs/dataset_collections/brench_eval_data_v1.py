7e89899968950bc34c180ab6d11894a1b9e9a891
Brench <148740663+BrenchCC@users.noreply.github.com>
2024-11-21 14:52:38 +0800

    Create brench_eval_data_v1.py

---
+++ configs/dataset_collections/brench_eval_data_v1.py
@@ -0,0 +1,10 @@
+from mmengine.config import read_base
+
+with read_base():
+    from opencompass.configs.datasets.mmlu.mmlu_gen_4d595a import mmlu_datasets
+    #from opencompass.configs.datasets.gsm8k.gsm8k_gen_1d7fe4 import gsm8k_datasets
+    from opencompass.configs.datasets.gsm8k.brench_eval_gsm8k import gsm8k_datasets
+    from opencompass.configs.datasets.math.math_gen_1ed9c2 import math_datasets
+    from opencompass.configs.dataset.humaneval.humaneval_gen_66a7f4 import humaneval_datasets
+
+datasets = sum((v for k, v in locals().items() if k.endswith('_datasets')), [])
