b274a84f78abbcd887b8479d501bcac4fcc8a728
brench <1034594842@qq.com>
2025-03-07 11:53:23 +0800

    update for math eval for qwen2.5

--- opencompass/configs/datasets/math/math_500_gen.py
+++ opencompass/configs/datasets/math/math_500_gen.py
@@ -19,7 +19,7 @@ math_infer_cfg = dict(
         ),
     ),
     retriever=dict(type=ZeroRetriever),
-    inferencer=dict(type=GenInferencer),
+    inferencer=dict(type=GenInferencer,max_out_len=16384),
 )
 
 
